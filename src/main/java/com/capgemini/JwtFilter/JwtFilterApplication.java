0a518423d5c460aa04e1f713b341b5bc0f49ab34
BigCarrasco <m.carrasco94@gmail.com>
2025-03-31 15:37:34 -0600

    fixing my test

--- src/main/java/com/capgemini/JwtFilter/JwtFilterApplication.java
+++ src/main/java/com/capgemini/JwtFilter/JwtFilterApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JwtFilterApplication {
 
 	public static void main(String[] args) {
-		testing to make my ci/cd fail
+
 		SpringApplication.run(JwtFilterApplication.class, args);
 	}
 
