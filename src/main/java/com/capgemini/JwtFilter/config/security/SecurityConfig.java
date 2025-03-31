5023c5d63ae06c84e0db0a1663e9641894f8fa1a
BigCarrasco <m.carrasco94@gmail.com>
2025-03-31 14:07:09 -0600

    security config allows 429 exception

--- src/main/java/com/capgemini/JwtFilter/config/security/SecurityConfig.java
+++ src/main/java/com/capgemini/JwtFilter/config/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.capgemini.JwtFilter.config.security;
 
 import com.capgemini.JwtFilter.config.filter.JwtAuthenticationFilter;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -23,7 +24,14 @@ public class SecurityConfig {
                         .requestMatchers("/auth/**","/test/**", "/rateLimiter/**", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
                 )
-                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(ex -> ex
+                    .authenticationEntryPoint((request, response, authException) -> {
+                        if (response.getStatus() == 429) {
+                            response.setStatus(429);
+                        }
+                    })
+                );
 
         return http.build();
     }
